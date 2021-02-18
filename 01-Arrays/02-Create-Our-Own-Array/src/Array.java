539720783243c1564a70861c2e3e282d2761da9e
Jakmao <maoxiaojie@yunfarm.cn>
2021-02-18 11:30:11 +0800

    Array

---
+++ 01-Arrays/02-Create-Our-Own-Array/src/Array.java
@@ -0,0 +1,34 @@
+/**
+ * Created by mxj on 2021/2/18 11:19 上午
+ */
+public class Array {
+    // 类的成员变量设置为private属性 保护类的封装性
+    private int[] data;
+    private int size;
+
+    // 构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    // 无参数的构造函数，默认数组的容量capacity=10
+    public Array() {
+        this(10);
+    }
+
+    // 获取数组的容量
+    public int getCapacity() {
+        return data.length;
+    }
+
+    // 获取数组中元素个数
+    public int getSize() {
+        return size;
+    }
+
+    // 返回数组是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
