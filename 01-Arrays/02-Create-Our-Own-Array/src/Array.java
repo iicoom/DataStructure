d7db6a7b58507e80f8d93ddfd7b2b249d9a8cddb
Jakmao <maoxiaojie@yunfarm.cn>
2021-02-20 20:46:09 +0800

    array method

--- 01-Arrays/02-Create-Our-Own-Array/src/Array.java
+++ 01-Arrays/02-Create-Our-Own-Array/src/Array.java
@@ -42,7 +42,7 @@ public class Array {
         if (size == data.length)
             throw new IllegalArgumentException("Add failed. Array is full");
 
-        if (index < 0 || index > size)
+        if (index < 0 || index > size) // 这里 index > size 保证添加到data的元素是紧密排列的
             throw new IllegalArgumentException("Add failed. Require index >=0 and index <=size");
 
         for (int i = size - 1; i >= index; i --) {
@@ -66,5 +66,113 @@ public class Array {
      */
     public void shift(int e) {
         add(0, e);
+    }
+
+    /**
+     * 获取data传入索引位置的元素
+     * @param index
+     * @return
+     */
+    int get(int index) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Get failed. index is Illegal");
+        return data[index];
+    }
+
+    /**
+     * 设置data传入索引位置的元素
+     * @param index
+     * @param e
+     */
+    void set(int index, int e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Get failed. index is Illegal");
+        data[index] = e;
+    }
+
+    /**
+     * 检测数组中是否存在传入的元素
+     * @param e
+     * @return
+     */
+    public boolean contains(int e) {
+        for (int i = 0; i < size; i ++) {
+            if (data[i] == e)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * 查找传入元素在data中的索引
+     * @param e
+     * @return
+     */
+    public int find(int e) {
+        for (int i = 0; i < size; i ++) {
+            if (data[i] == e)
+                return i;
+        }
+        return -1;
+    }
+
+    /**
+     * 从数组中删除指定位置的元素
+     * @param index
+     * @return
+     */
+    public int remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Add failed. Require index >=0 and index <=size");
+
+        int ret = data[index];
+        for (int i = index; i < size - 1; i ++) {
+            data[i] = data[i + 1];
+        }
+        size --;
+        return ret;
+    }
+
+    /**
+     * 删除数组中的第一个元素
+     * @return
+     */
+    public int unshift() {
+        return remove(0);
+    }
+
+    /**
+     * 删除数组中最后一个元素
+     * @return
+     */
+    public int pop() {
+        return remove(size - 1);
+    }
+
+    /**
+     * 如果数组中存在传入的元素则删除
+     * 如果data中有重复元素 此方法只会删除第一个e
+     * @param e
+     */
+    public void removeElement(int e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
+        res.append("[");
+        for (int i = 0; i < size; i ++) {
+            res.append(data[i]);
+            if (i != size - 1)
+                res.append(", ");
+        }
+        res.append("]");
+        return res.toString();
     }
 }
--- 01-Arrays/02-Create-Our-Own-Array/src/Main.java
+++ 01-Arrays/02-Create-Our-Own-Array/src/Main.java
@@ -2,5 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Array arr = new Array(20);
+        for (int i = 0; i < 10; i ++) {
+            arr.push(i);
+        }
+        System.out.println(arr);
+
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.shift(-1);
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
     }
 }
